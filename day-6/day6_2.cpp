e7f20e44959ce598399dc607dfc33b0f2fee0cf3
ixmorrow <ivanmorrow14@gmail.com>
2024-03-29 06:04:32 -0500

    refactor day6 pt 2

--- day-6/day6_2.cpp
+++ day-6/day6_2.cpp
@@ -8,83 +8,91 @@
 
 using namespace std;
 
-long long getData(string line){
-    std::size_t colonIndex = line.find(':');
-    if (colonIndex == std::string::npos) {
-        // Handle error: colon not found in the input line
-        std::cerr << "Error: Colon not found in the input line." << std::endl;
-        return 0;
-    }
+class RaceData {
+    public:
+        RaceData() { }
 
-    string line_of_data = line.substr(colonIndex+2);
+        void race(const string filename) {
+            // read file
+            ifstream myfile(filename);
 
-    // constructing stream from the string
-    string data_point;
-    string result;
-    stringstream ss(line_of_data);
-    while(ss >> data_point){
-        try {
-            result += data_point;
-        } catch (const std::out_of_range& e) {
-            // Handle error: The number is too large
-            std::cerr << "Error: Conversion failed. The number is too large." << std::endl;
-            break;
-        } catch (const std::invalid_argument& e) {
-            // Handle error: Invalid argument (not a valid integer)
-            std::cerr << "Error: Conversion failed. Invalid argument." << std::endl;
-            break;
+            if (!myfile.is_open()) {
+                cerr << "Error: Unable to open file '" << filename << "'" << endl;
+                throw "Unable to opepn file!";
+            }
+
+            // get the seed values from the first two lines of the text file
+            std::string line;
+            std::getline(myfile, line);
+            auto time = _get_data(line);
+            std::getline(myfile, line);
+            auto distance = _get_data(line);
+
+            int winPossibilities = _ways_to_win(time, distance);
+            cout << "Win Possibilities: " << winPossibilities << endl;
         }
-    }
 
-    
-    return stoll(result);
-}
+    private:
+        long long _get_data(string line){
+            std::size_t colonIndex = line.find(':');
+            if (colonIndex == std::string::npos) {
+                // Handle error: colon not found in the input line
+                std::cerr << "Error: Colon not found in the input line." << std::endl;
+                return 0;
+            }
 
-int waysToWinRace(long long timeInMs, long long longestDistanceInMm){
-    long long chargeTime = 0;
-    long long speed = 0;
-    int winCount = 0;
+            string line_of_data = line.substr(colonIndex+2);
 
-    while(chargeTime < timeInMs){
-        chargeTime++;
-        speed++;
-        long long timeLeft = timeInMs - chargeTime;
-        long long travelDistance = speed * timeLeft;
-        // cout << "Travel Distance: " << travelDistance << endl;
-        // cout << "Longest Distance: " << longestDistanceInMm << endl;
-        if(travelDistance > longestDistanceInMm) winCount++;
-    }
+            // constructing stream from the string
+            string data_point;
+            string result;
+            stringstream ss(line_of_data);
+            while(ss >> data_point){
+                try {
+                    result += data_point;
+                } catch (const std::out_of_range& e) {
+                    // Handle error: The number is too large
+                    std::cerr << "Error: Conversion failed. The number is too large." << std::endl;
+                    break;
+                } catch (const std::invalid_argument& e) {
+                    // Handle error: Invalid argument (not a valid integer)
+                    std::cerr << "Error: Conversion failed. Invalid argument." << std::endl;
+                    break;
+                }
+            }
+            return stoll(result);
+        }
 
-    return winCount;
-}
+        int _ways_to_win(long long timeInMs, long long longestDistanceInMm){
+            long long chargeTime = 0;
+            long long speed = 0;
+            int winCount = 0;
 
-void run(const string filename) {
+            while(chargeTime < timeInMs){
+                chargeTime++;
+                speed++;
+                long long timeLeft = timeInMs - chargeTime;
+                long long travelDistance = speed * timeLeft;
+                if(travelDistance > longestDistanceInMm) winCount++;
+            }
+
+            return winCount;
+        }
+};
+
+int main(int argc, char *argv[]) {
     // read file
-    ifstream myfile(filename);
-
-    if (!myfile.is_open()) {
-        cerr << "Error: Unable to open file '" << filename << "'" << endl;
-        throw "Unable to opepn file!";
+    string filename;
+    if (argc != 2) {
+        cout << "Filename not provided. Using default file input.txt..." << endl;
+        filename = "input.txt";
+    } else {
+        filename = argv[1];
     }
 
-    // get the seed values from the first two lines of the text file
-    std::string line;
-    std::getline(myfile, line);
-    auto time = getData(line);
-    std::getline(myfile, line);
-    auto distance = getData(line);
-
-    int winPossibilities = waysToWinRace(time, distance);
-
-
-    cout << "Win Possibilities: " << winPossibilities << endl;
-}
-
-int main() {
-    const string filename = "./input.txt";
-
+    RaceData race = RaceData();
     try{
-        run(filename);
+        race.race(filename);
     } catch (const char* msg) {
         cerr << msg << endl;
     }
