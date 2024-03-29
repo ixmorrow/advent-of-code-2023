e968b1b5c4499c900984154304a96798e7315bc0
ixmorrow <ivanmorrow14@gmail.com>
2024-03-29 06:17:54 -0500

    refactor day7 pt 1

--- day-7/day7.cpp
+++ day-7/day7.cpp
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+
 enum class Hand_Type {
     FiveOfAKind, // 0
     FourOfAKind, // 1
@@ -28,154 +29,170 @@ struct Hand {
     // Constructor
     Hand(string h, int m, Hand_Type ht) : hand(h), multiplier(m), hand_type(ht), winnings(0) {}
 };
+class CamelCards {
+    public:
+        CamelCards() { }
 
-unordered_map<char, int> cardRankings = 
-    {
-        {'A', 14},
-        {'K', 13},
-        {'Q', 12},
-        {'J', 11},
-        {'T', 10},
-        {'9', 9},
-        {'8', 8},
-        {'7', 7},
-        {'6', 6},
-        {'5', 5},
-        {'4', 4},
-        {'3', 3},
-        {'2', 2}
-    };
+        void play(const string filename) {
+            auto hands = buildHands(filename);
 
-bool compareCard(char card1, char card2){
-    return (cardRankings[card1] < cardRankings[card2]);
-}
+            // sorted list of hands
+            list<Hand> sorted_hands;
+            for(auto hand : hands){
+                _sort_hands(hand, sorted_hands);
+            }
 
-void sortHands(const Hand& hand, std::list<Hand>& hands) {
-    bool breakout = false;
+            int total_winnings = 0;
+            int rank = 1;
+            for (auto hand : sorted_hands) {
+                hand.winnings = hand.multiplier * rank;
+                total_winnings += hand.winnings;
 
-    if (hands.empty()) {
-        hands.push_back(hand);
-    } else {
-        auto it = hands.begin();
+                // cout << "Rank: " << rank << ", Hand: " << hand.hand << ", Multiplier: " << hand.multiplier << ", Winnings: " << hand.winnings << ", Type: " << static_cast<int>(hand.hand_type) << endl;
+                rank++;
+            }
 
-        while (it != hands.end() && it->hand_type >= hand.hand_type && !breakout) {
-            if (it->hand_type == hand.hand_type) {
-                // Iterate over each individual card
-                int i;
-                for (i = 0; i < 5; i++) {
-                    if (hand.hand[i] == it->hand[i]) {
-                        // If the cards are equal, move on to the next one
-                        continue;
-                    } else {
-                        if (compareCard(hand.hand[i], it->hand[i])) {
-                            breakout = true;
-                        } else {
+            cout << "Total Winnings: " << total_winnings << endl;
+        }
+
+    private:
+        unordered_map<char, int> cardRankings = 
+        {
+            {'A', 14},
+            {'K', 13},
+            {'Q', 12},
+            {'J', 11},
+            {'T', 10},
+            {'9', 9},
+            {'8', 8},
+            {'7', 7},
+            {'6', 6},
+            {'5', 5},
+            {'4', 4},
+            {'3', 3},
+            {'2', 2}
+        };
+
+        vector<Hand> buildHands(const string filename){
+            // read file
+            ifstream myfile(filename);
+
+            if (!myfile.is_open()) {
+                cerr << "Error: Unable to open file '" << filename << "'" << endl;
+                throw "Unable to opepn file!";
+            }
+
+            vector<Hand> hands;
+            std::string line;
+            while(getline(myfile, line)){
+                std::istringstream iss(line);
+                string hand;
+                int multiplier;
+
+                if (iss >> hand >> multiplier) {
+                    Hand hand_struct = {hand, multiplier, _calculate_hand(hand)};
+                    hands.push_back(hand_struct);
+                } else {
+                    // Handle error if the extraction fails
+                    std::cerr << "Error extracting data from the line." << std::endl;
+                }
+            }
+
+            return hands;
+        }
+
+        Hand_Type _calculate_hand(string cards){
+            unordered_map<char, int> card_count;
+            for(auto card : cards) {
+                if(card_count.find(card) != card_count.end()) {
+                    card_count[card] += 1;
+                }
+                else card_count[card] = 1;
+            }
+
+            int fullHouse = 0;
+            int threeOfKind = 0;
+            int twoPair = 0;
+            int onePair = 0;
+
+            for(auto it = card_count.begin(); it != card_count.end(); it++){
+                if(it->second == 5) return Hand_Type::FiveOfAKind;
+                else if(it->second == 4) return Hand_Type::FourOfAKind;
+                else if (it->second == 3){
+                    if(onePair == 1) return Hand_Type::FullHouse;
+                    else threeOfKind = 1;
+                }
+                else if (it->second == 2){
+                    if(threeOfKind == 1) return Hand_Type::FullHouse;
+                    else if(onePair == 1) return Hand_Type::TwoPair;
+                    else onePair = 1;
+                }
+            }
+
+            if(threeOfKind == 1) return Hand_Type::ThreeOfAKind;
+            else if (onePair == 1) return Hand_Type::OnePair;
+            else return Hand_Type::HighCard;
+        }
+
+        void _sort_hands(const Hand& hand, std::list<Hand>& hands) {
+            bool breakout = false;
+
+            if (hands.empty()) {
+                hands.push_back(hand);
+            } else {
+                auto it = hands.begin();
+
+                while (it != hands.end() && it->hand_type >= hand.hand_type && !breakout) {
+                    if (it->hand_type == hand.hand_type) {
+                        // Iterate over each individual card
+                        int i;
+                        for (i = 0; i < 5; i++) {
+                            if (hand.hand[i] == it->hand[i]) {
+                                // If the cards are equal, move on to the next one
+                                continue;
+                            } else {
+                                if (_compare_card(hand.hand[i], it->hand[i])) {
+                                    breakout = true;
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+
+                        // Break out of the loop if all cards are equal
+                        if (i == 5) {
                             break;
                         }
                     }
+
+                    if (!breakout) {
+                        ++it;
+                    }
                 }
 
-                // Break out of the loop if all cards are equal
-                if (i == 5) {
-                    break;
-                }
-            }
-
-            if (!breakout) {
-                ++it;
+                hands.insert(it, hand);
             }
         }
 
-        hands.insert(it, hand);
-    }
-}
-
-Hand_Type calculateHand(string cards){
-    unordered_map<char, int> card_count;
-    for(auto card : cards) {
-        if(card_count.find(card) != card_count.end()) card_count[card] += 1;
-        else card_count[card] = 1;
-    }
-
-    int fullHouse = 0;
-    int threeOfKind = 0;
-    int twoPair = 0;
-    int onePair = 0;
-
-    for(auto it = card_count.begin(); it != card_count.end(); it++){
-        if(it->second == 5) return Hand_Type::FiveOfAKind;
-        else if(it->second == 4) return Hand_Type::FourOfAKind;
-        else if (it->second == 3){
-            if(onePair == 1) return Hand_Type::FullHouse;
-            else threeOfKind = 1;
+        bool _compare_card(char card1, char card2){
+            return (cardRankings[card1] < cardRankings[card2]);
         }
-        else if (it->second == 2){
-            if(threeOfKind == 1) return Hand_Type::FullHouse;
-            else if(onePair == 1) return Hand_Type::TwoPair;
-            else onePair = 1;
-        }
-    }
 
-    if(threeOfKind == 1) return Hand_Type::ThreeOfAKind;
-    else if (onePair == 1) return Hand_Type::OnePair;
-    else return Hand_Type::HighCard;
-}
+};
 
-vector<Hand> buildHands(const string filename){
+int main(int argc, char *argv[]) {
     // read file
-    ifstream myfile(filename);
-
-    if (!myfile.is_open()) {
-        cerr << "Error: Unable to open file '" << filename << "'" << endl;
-        throw "Unable to opepn file!";
+    string filename;
+    if (argc != 2) {
+        cout << "Filename not provided. Using default file input.txt..." << endl;
+        filename = "input.txt";
+    } else {
+        filename = argv[1];
     }
 
-    vector<Hand> hands;
-    std::string line;
-    while(getline(myfile, line)){
-        std::istringstream iss(line);
-        string hand;
-        int multiplier;
-
-        if (iss >> hand >> multiplier) {
-            Hand hand_struct = {hand, multiplier, calculateHand(hand)};
-            hands.push_back(hand_struct);
-        } else {
-            // Handle error if the extraction fails
-            std::cerr << "Error extracting data from the line." << std::endl;
-        }
-    }
-
-    return hands;
-}
-
-void run(const string filename) {
-    auto hands = buildHands(filename);
-
-    // sorted list of hands
-    list<Hand> sorted_hands;
-    for(auto hand : hands){
-        sortHands(hand, sorted_hands);
-    }
-
-    int total_winnings = 0;
-    int rank = 1;
-    for (auto hand : sorted_hands) {
-        hand.winnings = hand.multiplier * rank;
-        total_winnings += hand.winnings;
-
-        // cout << "Rank: " << rank << ", Hand: " << hand.hand << ", Multiplier: " << hand.multiplier << ", Winnings: " << hand.winnings << ", Type: " << static_cast<int>(hand.hand_type) << endl;
-        rank++;
-    }
-
-    cout << "Total Winnings: " << total_winnings << endl;
-}
-
-int main() {
-    const string filename = "./input.txt";
-
+    CamelCards camel_cards = CamelCards();
     try{
-        run(filename);
+        camel_cards.play(filename);
     } catch (const char* msg) {
         cerr << msg << endl;
     }
