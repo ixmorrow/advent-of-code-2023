b57309376a83187add7f682e2e1042236cf4e71b
ixmorrow <ivanmorrow14@gmail.com>
2024-03-29 05:49:25 -0500

    refactor day5

--- day-5/day5.cpp
+++ day-5/day5.cpp
@@ -11,149 +11,202 @@
 
 using namespace std;
 
-vector<long long> getSeeds(string line){
-    vector<long long> seed_vals;
-    std::size_t colonIndex = line.find(':');
-    if (colonIndex == std::string::npos) {
-        // Handle error: colon not found in the input line
-        std::cerr << "Error: Colon not found in the input line." << std::endl;
-        return seed_vals;  // Return an empty vector indicating an error
-    }
-
-    string seeds = line.substr(colonIndex+2);
-
-    // constructing stream from the string
-    string seed;
-    stringstream ss(seeds);
-    while(ss >> seed){
-        try {
-            // Convert the string to a long long and add to the vector
-            seed_vals.push_back(std::stoll(seed));
-        } catch (const std::out_of_range& e) {
-            // Handle error: The number is too large
-            std::cerr << "Error: Conversion failed. The number is too large." << std::endl;
-            seed_vals.clear();  // Clear the vector to indicate an error
-            break;
-        } catch (const std::invalid_argument& e) {
-            // Handle error: Invalid argument (not a valid integer)
-            std::cerr << "Error: Conversion failed. Invalid argument." << std::endl;
-            seed_vals.clear();  // Clear the vector to indicate an error
-            break;
-        }
-    }
-
-    return seed_vals;
-}
-
-// Function to read a map from the file
-std::vector<std::vector<long long>> readMap(std::ifstream& inputFile) {
-    std::vector<std::vector<long long>> map;
-    std::string line;
-
-    if(line.empty()) getline(inputFile, line);
-
-    while (getline(inputFile, line) && !line.empty()) {
-        std::vector<long long> values;
-        std::istringstream iss(line);
-        
-        long long value;
-        while (iss >> value) {
-            values.push_back(value);
+class SeedMapper{
+    public:
+        SeedMapper(const std::string& myfile) {
+            _parseFile(myfile);
+            _lowest_location = std::numeric_limits<long long>::max(); // Initialize to maximum possible int value;
         }
 
-        map.push_back(values);
-    }
+        // Define vectors to store the data
+        std::vector<std::vector<long long>> _seedToSoilMap;
+        std::vector<std::vector<long long>> _soilToFertilizerMap;
+        std::vector<std::vector<long long>> _fertilizerToWaterMap;
+        std::vector<std::vector<long long>> _waterToLightMap;
+        std::vector<std::vector<long long>> _lightToTemperatureMap;
+        std::vector<std::vector<long long>> _temperatureToHumidityMap;
+        std::vector<std::vector<long long>> _humidityToLocationMap;
 
-    return map;
-}
+        vector<long long> get_seeds(){
+            return _seed_vals;
+        }
 
-long long getDestinationValue(long long src_value, vector<vector<long long>> map){
-    if(map.empty()) return 0;
+        long long get_lowest_destination_value(){
+            return _lowest_location;
+        }
 
-    long long destination = 0;
+        void calculate_lowest_location(){
+            for(auto seed : _seed_vals){
+                cout << "Seed: " << seed << endl;
+                long long soilValue = _getDestinationValue(seed, _seedToSoilMap);
+                long long fertilizerValue = _getDestinationValue(soilValue, _soilToFertilizerMap);
+                long long waterValue = _getDestinationValue(fertilizerValue, _fertilizerToWaterMap);
+                long long lightValue = _getDestinationValue(waterValue, _waterToLightMap);
+                long long tempValue = _getDestinationValue(lightValue, _lightToTemperatureMap);
+                long long humidityValue = _getDestinationValue(tempValue, _temperatureToHumidityMap);
+                long long location = _getDestinationValue(humidityValue, _humidityToLocationMap);
 
-    for(auto vec : map){
-        if (vec.size() >= 3){
-            long long range = vec[2];
-            long long sourceUpperBound = vec[1] + range;
-            long long sourceLowerBound = vec[1];
-
-            // val outside given range
-            if(sourceLowerBound > src_value || src_value > sourceUpperBound){
-                destination = src_value;
-            } else {
-                long long distance = src_value - sourceLowerBound;
-                destination = vec[0] + distance;
-                break;
+                _lowest_location = min(location, _lowest_location);
             }
         }
-    }
 
-    return destination;
-}
+    private:
+        vector<long long> _seed_vals;
+        long long _lowest_location;
 
-void run(const string filename) {
+        
+        void _getSeeds(string line){
+            std::size_t colonIndex = line.find(':');
+            if (colonIndex == std::string::npos) {
+                // Handle error: colon not found in the input line
+                std::cerr << "Error: Colon not found in the input line." << std::endl;
+            }
+
+            string seeds = line.substr(colonIndex+2);
+            // constructing stream from the string
+            string seed;
+            stringstream ss(seeds);
+            while(ss >> seed){
+                try {
+                    // Convert the string to a long long and add to the vector
+                    _seed_vals.push_back(std::stoll(seed));
+                } catch (const std::out_of_range& e) {
+                    // Handle error: The number is too large
+                    std::cerr << "Error: Conversion failed. The number is too large." << std::endl;
+                    _seed_vals.clear();  // Clear the vector to indicate an error
+                    break;
+                } catch (const std::invalid_argument& e) {
+                    // Handle error: Invalid argument (not a valid integer)
+                    std::cerr << "Error: Conversion failed. Invalid argument." << std::endl;
+                    _seed_vals.clear();  // Clear the vector to indicate an error
+                    break;
+                }
+            }
+        }
+        
+        void _parseFile(const std::string& filename) {
+            std::ifstream inputFile(filename);
+            if (!inputFile.is_open()) {
+                std::cerr << "Error: Unable to open file '" << filename << "'" << std::endl;
+                return;
+            }
+
+            std::string line;
+
+            // Parse seeds section
+            if (!std::getline(inputFile, line)) {
+                std::cerr << "Error: Unable to read file '" << filename << "'" << std::endl;
+                return;
+            }
+            _parseSeeds(line);
+
+            // Continue parsing other sections...
+            while (std::getline(inputFile, line)) {
+                if (line.empty()) {
+                    // Skip empty lines
+                    continue;
+                }
+
+                if (line.find("seed-to-soil map:") != std::string::npos) {
+                    cout << "Reading seed-to-soil map" << endl;
+                    // Parse seed-to-soil map section
+                    readMap(inputFile, _seedToSoilMap);
+                } else if (line.find("fertilizer-to-water map:") != std::string::npos) {
+                    cout << "Reading fertilizer-to-water map" << endl;
+                    // Parse fertilizer-to-water map section
+                    readMap(inputFile, _fertilizerToWaterMap);
+                } else if (line.find("water-to-light map:") != std::string::npos) {
+                    cout << "Reading water-to-light map" << endl;
+                    // Parse water-to-light map section
+                    readMap(inputFile, _waterToLightMap);
+                } else if (line.find("light-to-temperature map:") != std::string::npos) {
+                    cout << "Reading light-to-temperature map" << endl;
+                    // Parse light-to-temperature map section
+                    readMap(inputFile, _lightToTemperatureMap);
+                } else if (line.find("temperature-to-humidity map:") != std::string::npos) {
+                    cout << "Reading temp-to-humidity map" << endl;
+                    // Parse temperature-to-humidity map section
+                    readMap(inputFile, _temperatureToHumidityMap);
+                } else if (line.find("humidity-to-location map:") != std::string::npos) {
+                    cout << "Reading humidity-to-location map" << endl;
+                    // Parse humidity-to-location map section
+                    readMap(inputFile, _humidityToLocationMap);
+                }
+            }
+        }
+
+        void _parseSeeds(string line){
+            // Parse seeds section
+                cout << "Reading seeds" << endl;
+                std::istringstream iss(line.substr(line.find(":") + 1));
+                long long value;
+                while (iss >> value) {
+                    _seed_vals.push_back(value);
+                }
+        }
+
+        // Function to read a map from the file
+        void readMap(std::istream& input, std::vector<std::vector<long long>>& map) {
+            std::string line;
+            while (std::getline(input, line) && !line.empty()) {
+                std::istringstream iss(line);
+                std::vector<long long> values;
+                long long value;
+                while (iss >> value) {
+                    values.push_back(value);
+                }
+                map.push_back(values);
+            }
+        }
+
+        long long _getDestinationValue(long long src_value, vector<vector<long long>>& map){
+            if(map.empty()) {
+                cout << "Destination map is empty. Returning 0" << endl;
+                return 0;
+            }
+
+            long long destination = 0;
+
+            for(auto vec : map){
+                if (vec.size() >= 3){
+                    long long range = vec[2];
+                    long long sourceUpperBound = vec[1] + range;
+                    long long sourceLowerBound = vec[1];
+
+                    // val outside given range
+                    if(sourceLowerBound > src_value || src_value > sourceUpperBound){
+                        destination = src_value;
+                    } else {
+                        long long distance = src_value - sourceLowerBound;
+                        destination = vec[0] + distance;
+                        break;
+                    }
+                }
+            }
+            return destination;
+        }
+};
+
+int main(int argc, char *argv[]) {
     // read file
-    ifstream myfile(filename);
-
-    if (!myfile.is_open()) {
-        cerr << "Error: Unable to open file '" << filename << "'" << endl;
-        throw "Unable to opepn file!";
+    string filename;
+    if (argc != 2) {
+        cout << "Filename not provided. Using default file input.txt..." << endl;
+        filename = "input.txt";
+    } else {
+        filename = argv[1];
     }
 
-    // get the seed values from the first line of the text file
-    std::string line;
-    std::getline(myfile, line);
-    auto seeds = getSeeds(line);
+    SeedMapper seed_mapper = SeedMapper(filename);
+    seed_mapper.calculate_lowest_location();
 
-    // Define vectors to store the data
-    std::vector<std::vector<long long>> seedToSoilMap;
-    std::vector<std::vector<long long>> soilToFertilizerMap;
-    std::vector<std::vector<long long>> fertilizerToWaterMap;
-    std::vector<std::vector<long long>> waterToLightMap;
-    std::vector<std::vector<long long>> lightToTemperatureMap;
-    std::vector<std::vector<long long>> temperatureToHumidityMap;
-    std::vector<std::vector<long long>> humidityToLocationMap;
+    cout << "Lowest Location: " << seed_mapper.get_lowest_destination_value() << endl;
 
-    seedToSoilMap = readMap(myfile);
-    soilToFertilizerMap = readMap(myfile);
-    fertilizerToWaterMap = readMap(myfile);
-    waterToLightMap = readMap(myfile);
-    lightToTemperatureMap = readMap(myfile);
-    temperatureToHumidityMap = readMap(myfile);
-    humidityToLocationMap = readMap(myfile);
-
-    long long lowestLocation = std::numeric_limits<long long>::max(); // Initialize to maximum possible int value;
-
-    for(auto seed : seeds){
-        long long soilValue = getDestinationValue(seed, seedToSoilMap);
-
-        long long fertilizerValue = getDestinationValue(soilValue, soilToFertilizerMap);
-
-        long long waterValue = getDestinationValue(fertilizerValue, fertilizerToWaterMap);
-
-        long long lightValue = getDestinationValue(waterValue, waterToLightMap);
-
-        long long tempValue = getDestinationValue(lightValue, lightToTemperatureMap);
-
-        long long humidityValue = getDestinationValue(tempValue, temperatureToHumidityMap);
-
-        long long location = getDestinationValue(humidityValue, humidityToLocationMap);
-
-        lowestLocation = min(location, lowestLocation);
-    }
-
-    cout << "Lowest Location: " << lowestLocation << endl;
-}
-
-int main() {
-    const string filename = "./input.txt";
-
-    try{
-        run(filename);
-    } catch (const char* msg) {
-        cerr << msg << endl;
-    }
+    // for(int i=0; i<seed_mapper._fertilizerToWaterMap.size(); i++){
+    //     for(int j=0; j<seed_mapper._fertilizerToWaterMap[i].size(); j++){
+    //         cout << seed_mapper._fertilizerToWaterMap[i][j] << endl;
+    //     }
+    // }
 
     return 0;
 }
