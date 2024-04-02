a9b55f3f4c6247317ef840f0a621b876b4cf6ac3
ixmorrow <ivanmorrow14@gmail.com>
2024-04-01 22:18:29 -0500

    parse input data day8

--- day-4/day4.cpp
+++ day-4/day4.cpp
@@ -70,63 +70,6 @@ class ScratchCard {
         }
 };
 
-// void run(const string filename) {
-//     // read file
-//     ifstream myfile(filename);
-
-//     if (!myfile.is_open()) {
-//         cerr << "Error: Unable to open file '" << filename << "'" << endl;
-//         throw "Unable to opepn file!";
-//     }
-
-//     set<int> winning_numbers_count;
-//     int total_score = 0;
-
-//     string line;
-//     while(getline(myfile, line)) {
-//         int n = line.size();
-//         int colonIndex = line.find(':');
-//         int pipeDelimiter = line.find('|');
-
-//         string winning_numbers = line.substr(colonIndex+1, pipeDelimiter-colonIndex-1);
-//         string scratch_numbers = line.substr(pipeDelimiter+1, n);
-        
-//         // constructing stream from the string
-//         stringstream ss(winning_numbers);
-//         string num;
-
-//         while(getline(ss, num, ' ')){
-//             // Process num only if it's not whitespace
-//             if (!num.empty() && num.find_first_not_of(" \t\n\v\f\r") != std::string::npos) {
-//                 winning_numbers_count.insert(stoi(num));
-//             }
-//         }
-
-//         // constructing stream from the string
-//         stringstream ss2(scratch_numbers);
-//         bool shouldDouble = false;
-//         int game_score = 0;
-
-//         while(getline(ss2, num, ' ')){
-//             if (!num.empty() && num.find_first_not_of(" \t\n\v\f\r") != std::string::npos) {
-//                 int scratch_num = stoi(num);
-//                 if(winning_numbers_count.find(scratch_num) != winning_numbers_count.end()){
-//                     if(shouldDouble) game_score *= 2;
-//                     else {
-//                         game_score += 1;
-//                         shouldDouble = true;
-//                     }
-//                 }
-//             }
-//         }
-
-//         total_score += game_score;
-//         winning_numbers_count.clear();
-//     }
-
-//     cout << total_score << endl;
-// }
-
 int main(int argc, char *argv[]) {
     // read file
     string filename;
---
+++ day-8/day8.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+struct Node {
+        string current_node;
+        string left_node;
+        string right_node;
+};
+
+class Map {
+    public:
+        Map(const string filename) {
+            _build_map(filename);
+        };
+
+        vector<char> directions;
+        unordered_map<string, Node> nodes;
+
+        void print_nodes(){
+            for(const auto& pair : nodes){
+                cout << pair.first << ": "<< "(" << pair.second.left_node << " " << pair.second.right_node << ")" << endl;
+            }
+        }
+
+    private:
+        void _build_map(const string filename){
+            // read file
+            ifstream myfile(filename);
+
+            if (!myfile.is_open()) {
+                cerr << "Error: Unable to open file '" << filename << "'" << endl;
+                throw "Unable to opepn file!";
+            }
+
+            string line;
+            getline(myfile, line);
+            
+            for (char c : line) {
+                if (!std::isspace(c)) {
+                    directions.push_back(c);
+                }
+            }
+
+            while(getline(myfile, line)){
+                vector<string> node_strings = extractStrings(line);
+                Node node;
+                if (node_strings.size() == 3 && (nodes.find(node_strings[0]) == nodes.end())) {
+                    node.current_node = node_strings[0];
+                    node.left_node = node_strings[1];
+                    node.right_node = node_strings[2];
+                    nodes[node.current_node] = node;
+                }
+            }
+        };
+
+        // Function to extract strings of letters from a line
+        std::vector<std::string> extractStrings(const std::string& line) {
+            std::vector<std::string> strings;
+            std::stringstream ss(line);
+            std::string word;
+            while (ss >> word) {
+                std::string str;
+                for (char c : word) {
+                    if (isalpha(c)) {
+                        str += c;
+                    }
+                }
+                if (!str.empty()) {
+                    strings.push_back(str);
+                }
+            }
+            return strings;
+        }
+};
+
+int main(int argc, char *argv[]) {
+    // read file
+    string filename;
+    if (argc != 2) {
+        cout << "Filename not provided. Using default file input.txt..." << endl;
+        filename = "input.txt";
+    } else {
+        filename = argv[1];
+    }
+
+    Map map = Map(filename);
+    for(int i=0; i<map.directions.size(); i++){
+        cout << map.directions[i] << endl;
+    }
+
+    map.print_nodes();
+
+    return 0;
+}
